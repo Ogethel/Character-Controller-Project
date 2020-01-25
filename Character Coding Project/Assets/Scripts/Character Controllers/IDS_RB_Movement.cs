83bb7b7d09e6476021fe627b943a4c2bc8a69775
McKade <10718916@my.uvu.edu>
2020-01-25 16:46:38 -0700

    Adding the bones of a RB Movement script

---
+++ Character Coding Project/Assets/Scripts/Character Controllers/IDS_RB_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace IconicDesignStudios.Movement
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class IDS_RB_Movement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
