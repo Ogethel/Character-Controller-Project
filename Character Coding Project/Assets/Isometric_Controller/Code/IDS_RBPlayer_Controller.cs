3f230bc94c3738459da759d13dd916bd6470ebdc
McKade <10718916@my.uvu.edu>
2020-01-28 01:48:13 -0700

    Adding the Back bone of a Aiming system

---
+++ Character Coding Project/Assets/Isometric_Controller/Code/IDS_RBPlayer_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace IconicDesignStudios.Controller
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(IDS_RBPlayer_Inputs))]
+    public class IDS_RBPlayer_Controller : MonoBehaviour
+    {
+        private Rigidbody rb;
+        private IDS_RBPlayer_Inputs input;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            input = GetComponent<IDS_RBPlayer_Inputs>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
