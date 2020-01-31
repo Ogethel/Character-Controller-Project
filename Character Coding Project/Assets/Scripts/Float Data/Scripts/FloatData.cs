50fd8d3ea64d7bf2fffe7697ac8bc82553c34e18
McKade <10718916@my.uvu.edu>
2020-01-31 01:33:56 -0700

    Getting the Data scripts to work with the character mover

---
+++ Character Coding Project/Assets/Scripts/Float Data/Scripts/FloatData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Object = UnityEngine.Object;
+
+//Made By Anthony Romrell
+
+[ExecuteInEditMode]
+[CreateAssetMenu(menuName = "Single Variables/FloatData")]
+public class FloatData : ScriptableObject
+{
+    [SerializeField] protected float value;
+
+    public virtual float Value
+    {
+        get => value;
+        set => this.value = value;
+    }
+    
+    public void SetValue (float amount)
+    {
+        Value = amount;
+    }
+
+    public void UpdateValue(float amount)
+    {
+        Value += amount;
+    }
+
+    public void UpdateValue(Object data)
+    {
+        var newData = data as FloatData;
+        if (newData != null) Value += newData.Value;
+    }
+
+    public void SetValue(Object data)
+    {
+        var newData = data as FloatData;
+        if (newData != null) Value = newData.Value;
+    }
+    
+    public void CheckMinValue(float minValue)
+    {
+        if (Value <= minValue)
+        {
+            Value = minValue;
+        }
+    }
+
+    public void CheckMaxValue(float maxValue)
+    {
+        if (Value >= maxValue)
+        {
+            Value = maxValue;
+        }
+    }
+}
