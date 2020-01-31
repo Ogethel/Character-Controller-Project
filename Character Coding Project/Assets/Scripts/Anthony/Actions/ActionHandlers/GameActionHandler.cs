43648ee72889edd3d89da8a28148ef3455496810
McKade <10718916@my.uvu.edu>
2020-01-30 23:58:01 -0700

    Adding Actions Scripts from Anthony to play with

---
+++ Character Coding Project/Assets/Scripts/Anthony/Actions/ActionHandlers/GameActionHandler.cs
@@ -0,0 +1,20 @@
+﻿using ArtisanDream.Tools.Actions;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+    public class GameActionHandler : MonoBehaviour
+    {
+        public GameAction Action;
+        public UnityEvent Event;
+
+        private void OnEnable()
+        {
+            Action.RaiseNoArgs += Respond;
+        }
+
+        private void Respond()
+        {
+            Event.Invoke();
+        }
+    }
